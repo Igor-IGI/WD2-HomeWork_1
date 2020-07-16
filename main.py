0b90447f07716fdc3a0c402f3857d84c8c3d7615
Igor Popovic <igor.tkd9@gmail.com>
2020-07-16 12:53:09 +0200

    Add some prints for tables

--- main.py
+++ main.py
@@ -2,4 +2,26 @@ from smartninja_sql.sqlite import SQLiteDatabase
 
 db = SQLiteDatabase("Chinook_Sqlite.sqlite")
 
+#  Print names of artist in Artist table
 db.pretty_print("SELECT ArtistId, Name FROM Artist")
+
+#  Print all data from table Invoice where BillCountry is Germany
+db.pretty_print("""
+    SELECT * FROM Invoice
+    WHERE BillingCountry = "Germany"
+""")
+
+# Print the number of albums in the table
+db.pretty_print("""
+    SELECT COUNT(Title)
+    FROM Album
+""")
+
+db.pretty_print("""
+    SELECT COUNT(Country)
+    FROM Customer
+    WHERE Country ="France"
+""")
+
+
+
